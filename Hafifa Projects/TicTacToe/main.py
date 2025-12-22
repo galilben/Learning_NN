66e94ffb99fdd7c6c57556f4ab416d22622c9f0b
Galben <galbm2004@gmail.com>
2025-12-23 01:47:44 +0200

    better structure

--- Hafifa Projects/TicTacToe/main.py
+++ Hafifa Projects/TicTacToe/main.py
@@ -2,25 +2,7 @@ import tensorflow
 from GameFunctions import *
 from tensorflow import keras
 import numpy as np
-
-def random_move(state):
-    options=np.where(state==0)[0]
-    return np.random.choice(options)
-
-def train_with_random_game():
-    board = np.zeros(9)
-    boards_seen = []
-    moves = []
-    player=1
-    for i in range(9):
-        move = random_move(board)
-        boards_seen.append(board.copy())
-        moves.append(move)
-        board[move] = player
-        if check_winner(board):
-            return boards_seen, moves, player
-        player =player^1^2
-    return 0,0,0 #in case of draw doesnt matter
+from TicTacToe.training import *
 
 
 
